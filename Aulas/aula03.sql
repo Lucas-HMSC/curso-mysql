16b53a82b63c2842d4f98909cad756bf7f1d7b6d
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-10-12 19:16:54 -0300

    Class aula03 Completed

---
+++ Aulas/aula03.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE cadastro;
+
+CREATE TABLE pessoas (
+nome VARCHAR(30),
+idade TINYINT,
+sexo CHAR(1),
+peso FLOAT,
+altura FLOAT,
+nacionalidade VARCHAR(20)
+);
+
+DESCRIBE pessoas;
