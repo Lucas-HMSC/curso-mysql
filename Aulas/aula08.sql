8161c1423b547b0688b788a4aba2ffbdb104a41f
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-10-24 13:21:19 -0300

    Class aula08 Completed

---
+++ Aulas/aula08.sql
@@ -0,0 +1,13 @@
+USE cadastro;
+
+SHOW TABLES;
+
+SELECT * FROM cursos;
+SELECT * FROM gafanhotos;
+
+DESC gafanhotos;
+DESC cursos;
+
+DROP DATABASE cadastro;
+
+/*Banco de Dados recuperado utilizando DUMP*/ 
